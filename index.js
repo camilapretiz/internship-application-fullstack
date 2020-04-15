f71a603c87e0bdd550540917e6dfad04cfef355f
Camila Pretiz <cp573@cornell.edu>
2020-04-14 20:07:13 -0600

    Finished challenge + extra credit #1

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
 /**
- * Respond with hello worker text
+ * Handles incoming anchor elements.
+ */
+class AnchorElementHandler {
+  element(element) {
+    const attribute = element.getAttribute('href');
+    element.setAttribute(
+      'href',
+      attribute.replace('cloudflare.com', 'linkedin.com/in/camilapretiz')
+    );
+    element.setInnerContent('Go to Camila\'s Linkedin!');
+  }
+}
+
+const htmlRewriter = new HTMLRewriter().on('a#url', new AnchorElementHandler());
+
+/**
+ * Response with random variant page content.
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants');
+  const responseJson = await response.json();
+  const urls = responseJson['variants'];
+  const index = Math.floor(Math.random() * 2);
+  const variantResponse = await fetch(urls[index]);
+  return htmlRewriter.transform(variantResponse);
 }
